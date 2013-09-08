38b4eced8317b36d6912dd50fc95c69a896caaba
SUZUKI Kei <todokimasen@gmail.com>
2013-09-08 22:47:48 +0900

    Create project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4ActionControllerLiveWithGoogleMapsSample::Application.config.secret_key_base = '56ee3754bd4786d2a410f560de6274638466778b645e9939e1c14e98b494f06f18b1aef3cdd7868abc2d82a678b457b0c6adc3aa764291e7680c7b6f95172d89'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4ActionControllerLiveWithGoogleMapsSample::Application.config.session_store :cookie_store, key: '_rails4_action_controller_live_with_google_maps_sample_session'
